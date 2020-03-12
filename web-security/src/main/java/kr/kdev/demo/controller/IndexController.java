dd644486b9948aa852d32f4fa8b60c6405989151
kdevkr <kdevkr@gmail.com>
2020-03-13 00:13:54 +0900

    빌드 실패 코드 제거

--- web-security/src/main/java/kr/kdev/demo/controller/IndexController.java
+++ web-security/src/main/java/kr/kdev/demo/controller/IndexController.java
@@ -1,17 +1,8 @@
 package kr.kdev.demo.controller;
 
-import com.vladsch.flexmark.html.HtmlRenderer;
-import com.vladsch.flexmark.parser.Parser;
-import com.vladsch.flexmark.util.ast.Node;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 
 @Controller
